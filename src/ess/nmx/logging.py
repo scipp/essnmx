dd4b48e9d340102b8bc21bf4ac7f0d65d3722857
YooSunyoung <luysunyoung9@gmail.com>
2023-12-07 15:14:28 +0100

    Add logging providers.

---
+++ src/ess/nmx/logging.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+from logging import Logger
+from typing import NewType
+
+NMXLogger = NewType("NMXLogger", Logger)
+
+
+def get_logger() -> NMXLogger:
+    """Return scipp logger for logging nmx data reduction."""
+    from scipp.logging import get_logger
+
+    return NMXLogger(get_logger())
+
+
+logging_providers = [get_logger]
