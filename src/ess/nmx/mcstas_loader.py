e36a6ef2fe468f45c53ed58cffd5949b97ddebaa
YooSunyoung <luysunyoung9@gmail.com>
2023-12-20 16:58:19 +0100

    Geometry parsing draft.

--- src/ess/nmx/mcstas_loader.py
+++ src/ess/nmx/mcstas_loader.py
@@ -1,11 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
-from dataclasses import dataclass
 from typing import Iterable, NewType, Optional
 
+import numpy as np
 import scipp as sc
 import scippnexus as snx
-from defusedxml import ElementTree as ET
+from numpy.typing import NDArray
 
 PixelIDs = NewType("PixelIDs", sc.Variable)
 InputFilepath = NewType("InputFilepath", str)
@@ -15,14 +15,7 @@ NMXData = NewType("NMXData", sc.DataArray)
 MaximumProbability = NewType("MaximumProbability", int)
 DefaultMaximumProbability = MaximumProbability(100_000)
 
-
-@dataclass
-class McStasGeometry:
-    ds_l: list
-    fast_l: list
-    slow_l: list
-    sample_pos: list
-    source_pos: list
+DetectorGeometry = NewType("DetectorGeometry", sc.Dataset)
 
 
 def _retrieve_event_list_name(keys: Iterable[str]) -> str:
@@ -72,14 +65,12 @@ def _collect_components(xml_str: bytes) -> list:
     return [branch for branch in tree if branch.tag == 'component']
 
 
-def _select_by_type_prefix(
-    components: list[ET.Element], prefix: str
-) -> list[ET.Element]:
+def _select_by_type_prefix(components: list, prefix: str) -> list:
     """Select components by type prefix."""
     return [comp for comp in components if comp.attrib['type'].startswith(prefix)]
 
 
-def _check_if_only_one(xml_items: list[ET.Element], name: str) -> None:
+def _check_if_only_one(xml_items: list, name: str) -> None:
     """Check if there is only one element with ``name``."""
     if len(xml_items) > 1:
         raise ValueError(f"Multiple {name}s found.")
@@ -87,23 +78,23 @@ def _check_if_only_one(xml_items: list[ET.Element], name: str) -> None:
         raise ValueError(f"No {name} found.")
 
 
-def _retrieve_attribs(component: ET.Element, *args: str) -> list[float]:
+def _retrieve_attribs(component, *args: str) -> list[float]:
     """Retrieve ``args`` from xml."""
 
     return [float(component.attrib[key]) for key in args]
 
 
-def _retrieve_xyz(component: ET.Element) -> list[float]:
-    """Retrieve xyz from xml."""
+def _retrieve_xyz(component) -> sc.Variable:
+    """Retrieve x, y, z position from xml."""
     location = component.find('location')
     if location is None:
         raise ValueError("No location found in component ", component.find('name'))
 
-    return _retrieve_attribs(location, 'x', 'y', 'z')
+    return sc.vector(_retrieve_attribs(location, 'x', 'y', 'z'))
 
 
-def _retrieve_rxyz(component: ET.Element) -> list[float]:
-    """Retrieve rotation angle, x, y, z axes from xml."""
+def _retrieve_rotation_axis_angle(component) -> list[float]:
+    """Retrieve rotation angle(theta), x, y, z axes from xml."""
     location = component.find('location')
     if location is None:
         raise ValueError("No location found in component ", component.find('name'))
@@ -111,9 +102,91 @@ def _retrieve_rxyz(component: ET.Element) -> list[float]:
     return _retrieve_attribs(location, 'rot', 'axis-x', 'axis-y', 'axis-z')
 
 
-def load_mcstas_geometry(file_path: InputFilepath) -> McStasGeometry:
+def axis_angle_to_quaternion(
+    x: float, y: float, z: float, theta: sc.Variable
+) -> NDArray:
+    """Convert axis-angle to queternions, [x, y, z, w].
+
+    Parameters
+    ----------
+    x:
+        X component of axis of rotation.
+    y:
+        Y component of axis of rotation.
+    z:
+        Z component of axis of rotation.
+    theta:
+        Angle of rotation, with unit of ``rad`` or ``deg``.
+
+    Returns
+    -------
+    :
+        A list of (normalized) queternions, [x, y, z, w].
+
+    Notes
+    -----
+    Axis of rotation (x, y, z) does not need to be normalized,
+    but it returns a unit quaternion (x, y, z, w).
+
+    """
+
+    w: sc.Variable = sc.cos(theta.to(unit='rad') / 2)
+    xyz: sc.Variable = -sc.sin(theta.to(unit='rad') / 2) * sc.vector([x, y, z])
+    q = np.array([*xyz.values, w.value])
+    return q / np.linalg.norm(q)
+
+
+def quaternion_to_matrix(x: float, y: float, z: float, w: float) -> sc.Variable:
+    """Convert quaternion to rotation matrix.
+
+    Parameters
+    ----------
+    x:
+        x(a) component of quaternion.
+    y:
+        y(b) component of quaternion.
+    z:
+        z(c) component of quaternion.
+    w:
+        w component of quaternion.
+
+    Returns
+    -------
+    :
+        A 3X3 rotation matrix (3 vectors).
+
+    """
+    from scipy.spatial.transform import Rotation
+
+    return sc.spatial.rotations_from_rotvecs(
+        rotation_vectors=sc.vector(
+            Rotation.from_quat([x, y, z, w]).as_rotvec(),
+            unit='rad',
+        )
+    )
+
+
+def flip_vector(x: sc.Variable) -> sc.Variable:
+    """Flip a vector.
+
+    It is just multiplying -1 to each component,
+    but this function shows the purpose of multiplying -1.
+    It will be easier to find where the vectors are flipped in the process.
+
+    """
+    return x * -1
+
+
+def _retrieve_rotation_matrix_from_detector(detector) -> sc.Variable:
+    theta, x, y, z = _retrieve_rotation_axis_angle(detector)
+    q = axis_angle_to_quaternion(x, y, z, sc.scalar(theta, unit='deg'))
+    return quaternion_to_matrix(*q)
+
+
+def load_mcstas_geometry(file_path: InputFilepath) -> DetectorGeometry:
     """Retrieve geometry parameters from mcstas file"""
     import numpy as np
+    import scipp as sc
 
     xml = _read_mcstas_geometry_xml(file_path)
     components = _collect_components(xml)
@@ -123,37 +196,68 @@ def load_mcstas_geometry(file_path: InputFilepath) -> McStasGeometry:
     samples = _select_by_type_prefix(components, 'sampleMantid-type')
     _check_if_only_one(sources, 'source')
     _check_if_only_one(samples, 'sample')
-    sample_pos = np.array(_retrieve_xyz(samples.pop())) * [-1, -1, -1]
-    source_pos = np.array(_retrieve_xyz(sources.pop()))
 
-    fast_l = []
-    slow_l = []
-    vec_f = np.array([-1.0, 0.0, -0.0])
-    vec_s = np.array([0.0, -1.0, 0.0])
-    ds_l = []
+    sample_pos = flip_vector(_retrieve_xyz(samples.pop()))  # Why flipping?
+    # source_pos = _retrieve_xyz(sources.pop())  # Why not flipping?
 
-    for detector in detectors:
-        det_pos = np.array(_retrieve_xyz(detector)) * [-1, -1, -1]
-        rel_pos = np.round(det_pos - sample_pos, 2)
-        # rel_pos = np.round(twoP_to_vec(sample_pos, det_pos), 2)
-        ds_l.append(rel_pos)
+    fast_axis = flip_vector(sc.vector([1.0, 0.0, 0.0]))  # Why flipping? Hardcoded?
+    slow_axis = flip_vector(sc.vector([0.0, 1.0, 0.0]))  # Why flipping? Hardcoded?
 
-        rot_pos = np.array(_retrieve_rxyz(detector))
-        theta = np.radians(-rot_pos[0])
-        v = -(rot_pos[1:] / np.linalg.norm(rot_pos[1:])) * np.sin(theta / 2)
-        q = np.array([-np.cos(theta / 2)] + v.tolist())
-        w, x, y, z = q
-        rot_matrix = np.array(
-            [
-                [1 - 2 * (y**2 + z**2), 2 * (x * y - z * w), 2 * (x * z + y * w)],
-                [2 * (x * y + z * w), 1 - 2 * (x**2 + z**2), 2 * (y * z - x * w)],
-                [2 * (x * z - y * w), 2 * (y * z + x * w), 1 - 2 * (x**2 + y**2)],
-            ]
+    detectors_to_samples = [
+        sc.vector(
+            np.round((flip_vector(_retrieve_xyz(detector)) - sample_pos).values, 2)
         )
-        fast_l.append(np.round(np.dot(rot_matrix, vec_f), 2))
-        slow_l.append(np.dot(rot_matrix, vec_s))
+        # Why flipping? Why rounding?
+        for detector in detectors
+    ]
+    rotation_matrices = [
+        _retrieve_rotation_matrix_from_detector(detector) for detector in detectors
+    ]
+    slow_axes = [
+        rotation_matrix * slow_axis  # Why not rounding?
+        for rotation_matrix in rotation_matrices
+    ]
+    fast_axes = [
+        sc.vector(np.round((rotation_matrix * fast_axis).values, 2))  # Why rounding?
+        for rotation_matrix in rotation_matrices
+    ]
 
-    return ds_l, fast_l, slow_l, sample_pos, source_pos
+    return DetectorGeometry(
+        sc.Dataset(
+            coords={
+                'id': sc.arange('detector', len(detectors)),
+            },
+            data={
+                'origen': sc.concat(detectors_to_samples, 'detector'),
+                'slow_axis': sc.concat(slow_axes, 'detector'),
+                'fast_axis': sc.concat(fast_axes, 'detector'),
+            },
+        )
+    )
+
+
+def _retrieve_detector_geometries(tree):
+    """Retrieve detector geometries from mcstas file"""
+
+    components = [branch for branch in tree if branch.tag == 'component']
+    detectors = _select_by_type_prefix(components, 'MonNDtype')
+    type_list = [branch for branch in tree if branch.tag == 'type']
+
+    def _find_type_desc(elem, types: list):
+        for type_ in types:
+            if type_.attrib['name'] == elem.attrib['type']:
+                return type_
+
+        raise ValueError(f"Can not find type for component {elem.attrib['name']}.")
+
+    detector_type_map = {
+        det.attrib['name']: _find_type_desc(det, type_list) for det in detectors
+    }
+    detector_type_type_map = {
+        det_type.attrib['name']: _find_type_desc(det_type, type_list)
+        for det_type in detector_type_map.values()
+    }
+    return detector_type_map, detector_type_type_map
 
 
 def load_mcstas_nexus(
@@ -171,7 +275,10 @@ def load_mcstas_nexus(
         The maximum probability to scale the weights.
 
     """
+    from defusedxml.ElementTree import fromstring
 
+    tree = fromstring(_read_mcstas_geometry_xml(file_path))
+    _retrieve_detector_geometries(tree)
     probability = max_probability or DefaultMaximumProbability
 
     with snx.File(file_path) as file:
