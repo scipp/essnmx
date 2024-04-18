237c5acc704c0aa717f84a5ed6d5d61848489277
YooSunyoung <luysunyoung9@gmail.com>
2024-04-18 12:27:25 +0200

    Add quad root stadard deviation cutting.

--- src/ess/nmx/scaling.py
+++ src/ess/nmx/scaling.py
@@ -12,7 +12,8 @@ WavelengthBinSize = NewType("WavelengthBinSize", int)
 """The size of the wavelength(LAMBDA) bins."""
 WavelengthEdgeCut = NewType("WavelengthEdgeCut", float)
 """The proportional cut of the wavelength binned data. 0 < proportion < 0.5."""
-
+QuadRootStadardDeviationCut = NewType("QuadRootStadardDeviationCut", int)
+"""The number of standard deviations to be cut from the 4-th root data."""
 
 # Computed types
 WavelengthBinned = NewType("WavelengthBinned", sc.DataArray)
@@ -26,9 +27,15 @@ ScaleFactorIntensity = NewType("ScaleFactorIntensity", float)
 ScaleFactorSigmaIntensity = NewType("ScaleFactorSigmaIntensity", float)
 """The scale factor for the standard uncertainty of intensity."""
 WavelengthScaled = NewType("WavelengthScaled", sc.DataArray)
-"""Scaled wavelength by the reference bin."""
-WavelengthScaledTrimmed = NewType("WavelengthScaledTrimmed", sc.DataArray)
-"""Scaled wavelength by the reference bin with the edges cut."""
+"""Scaled intensity by the reference bin."""
+ScaledTrimmedIntensity = NewType("ScaledTrimmedIntensity", sc.DataArray)
+"""Scaled intensity by the reference bin with the edges cut."""
+IntensitySampleVariation = NewType("IntensitySampleVariation", sc.DataArray)
+"""The sample variation of the intensity."""
+FilteredIntensity = NewType("FilteredIntensity", sc.DataArray)
+"""The filtered intensity by the quad root of the sample standard deviation."""
+ScaledIntensity = NewType("ScaledIntensity", sc.DataArray)
+"""Scaled intensity within the desired certainty."""
 
 
 def _is_bin_empty(binned: sc.DataArray, idx: int) -> bool:
@@ -150,7 +157,7 @@ def scale_by_reference_bin(
 
 def cut_edges(
     scaled: WavelengthScaled, edges: WavelengthEdgeCut
-) -> WavelengthScaledTrimmed:
+) -> ScaledTrimmedIntensity:
     """Cut the edges of the scaled data.
 
     Parameters
@@ -168,8 +175,71 @@ def cut_edges(
 
     """
     cutting_index = int(edges * len(scaled))
-    return WavelengthScaledTrimmed(
+    return ScaledTrimmedIntensity(
         scaled[DEFAULT_WAVELENGTH_COLUMN_NAME, cutting_index:-cutting_index]
+    )
+
+
+def _calculate_sample_standard_deviation(var: sc.Variable) -> sc.Variable:
+    """Calculate the sample variation of the data.
+
+    This helper function is a temporary solution before
+    we release new scipp version with the statistics helper.
+    """
+    import numpy as np
+
+    return sc.scalar(np.nanstd(var.values))
+
+
+def _bin_edge_to_midpoint_coord(bin_edges: sc.Variable) -> sc.Variable:
+    """Convert the bin edges to the midpoint coordinates."""
+
+    return (bin_edges[1:] + bin_edges[:-1]) / 2
+
+
+def cut_by_quad_root_sample_std(
+    da: ScaledTrimmedIntensity, n_cut: QuadRootStadardDeviationCut
+) -> FilteredIntensity:
+    """Cut the data by the quad root of the sample standard deviation.
+
+    The data is flattened since the standard deviation cut
+    is not aligned with the original coordinates.
+
+    Parameters
+    ----------
+    da:
+        The scaled and trimmed data.
+
+    n_cut:
+        The number of standard deviations to be kept from mean.
+
+    """
+    copied = da.copy(deep=False)
+
+    # Make bin edge coordinate to normal coordinate.
+    copied.coords[DEFAULT_WAVELENGTH_COLUMN_NAME] = _bin_edge_to_midpoint_coord(
+        copied.coords[DEFAULT_WAVELENGTH_COLUMN_NAME]
+    )  # Bin edges are lost when they are flattened.
+    flattened = copied.flatten(dims=da.dims, to="row")
+    quad_root = flattened.data ** (0.25)
+
+    # Calculate the mean and standard deviation of the quad root
+    quad_root.variances = None
+    quad_root_mean = quad_root.nanmean()
+    quad_root_std = _calculate_sample_standard_deviation(quad_root)
+    half_window = n_cut * quad_root_std
+    keep_range = (quad_root_mean - half_window, quad_root_mean + half_window)
+
+    # Keep the data within the range
+    flattened.coords["keep"] = sc.logical_and(
+        quad_root >= keep_range[0], quad_root < keep_range[1]
+    )
+    keep_or_discard = flattened.group("keep")
+    if not sc.any(keep_or_discard.coords['keep']):
+        raise ValueError("No data fell into the keeping window. Try wider cut.")
+
+    return FilteredIntensity(
+        keep_or_discard["keep", sc.scalar(True)].values.copy(deep=False)
     )
 
 
@@ -180,5 +250,6 @@ scaling_providers = (
     calculate_scale_factor_per_hkl_eq,
     scale_by_reference_bin,
     cut_edges,
+    cut_by_quad_root_sample_std,
 )
 """Providers for scaling data."""
