a8d8e7ebc7274a2849157d273a4b96332e57dbe1
YooSunyoung <17974113+YooSunYoung@users.noreply.github.com>
2026-01-29 13:43:37 +0100

    Add duplicated file path test.

--- tests/executable_test.py
+++ tests/executable_test.py
@@ -510,3 +510,20 @@ def test_reduction_compression_bitshuffle_fall_back_to_gzip(
             data_path = f'entry/instrument/detector_panel_{i}/data'
             assert file[data_path].chunks == (1280, 1280, 1)
             assert file[data_path].compression == 'gzip'
+
+
+def test_reduction_two_files_duplicated_path(reduction_config: ReductionConfig) -> None:
+    # First run with one file.
+    with known_warnings():
+        single_file_results = reduction(config=reduction_config)
+
+    # Run with two files with same names. (same files)
+    reduction_config.inputs.input_file = reduction_config.inputs.input_file * 2
+    with known_warnings():
+        double_file_results = reduction(config=reduction_config)
+
+    for panel in single_file_results['histogram']:
+        single_data = single_file_results['histogram'][panel]
+        double_data = double_file_results['histogram'][panel]
+        # Duplicated file path should have been ignored.
+        assert_identical(double_data, single_data)
